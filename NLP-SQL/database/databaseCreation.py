e682fb2a64bccb952caa229769f4561ca6451246
Dmitry <61515881+blinov-89@users.noreply.github.com>
2022-11-13 09:47:09 +0300

    Update databaseCreation.py

--- NLP-SQL/database/databaseCreation.py
+++ NLP-SQL/database/databaseCreation.py
@@ -12,7 +12,7 @@ CREATE TABLE student (
  sId INTEGER PRIMARY KEY,
  name VARCHAR(20) NOT NULL,
  sex CHAR(7),
- address VARCHAR(50), 
+ address VARCHAR(50),
  phone CHAR(20),
  cgpa INTEGER
 );
@@ -26,9 +26,33 @@ CREATE TABLE department (
 );
 
 #Create users table
-CREATE TABLES users )
-	userid  VARCHAR(30) PRIMARY KEY
-	password VARCHAR(30)
-	access INTEGER
-	name VARCHAR(30)
+CREATE TABLES users (
+	userid  VARCHAR(30) PRIMARY KEY,
+	password VARCHAR(30),
+	access INTEGER,
+	name VARCHAR(30),
 );
+
+CREATE TABLE propuska (
+ID varchar(50),
+	Series varchar(50),
+ 	Number varchar(50),
+  	ValidFrom varchar(50),
+   	ValidTo varchar(50),
+    Zone varchar(50),
+    Status varchar(50),
+    CancelDate varchar(50),
+    global_id varchar(50)
+);
+CREATE TABLE sport(
+section varchar(50),
+	subsection varchar(50),
+ 	title varchar(50),
+  	description varchar(50),
+   	start varchar(50),
+    stop varchar(50),
+    addres varchar(50),
+    participant varchar(50)
+);
+
+
